f274a790347f08a178483f30d801dc1620bd246c
Andreandhiki <andreandhikiriyantap@mail.ugm.ac.id>
2026-08-28 16:30:24 +0700

    fix: benerin height biar ga scrollable

--- apps/web/src/components/sign-in-form.tsx
+++ apps/web/src/components/sign-in-form.tsx
@@ -157,7 +157,7 @@ export default function SignInForm({
   }
 
   return (
-    <div className="w-full flex items-center justify-center p-4 lg:min-h-[calc(100svh-4rem)]">
+    <div className="w-full flex items-center justify-center p-4 lg:min-h-[calc(100svh-6rem)]">
       <Card className="w-full lg:w-5/12 xl:w-md">
         <CardHeader align="center">
           <CardTitle>Sign in to your account</CardTitle>
